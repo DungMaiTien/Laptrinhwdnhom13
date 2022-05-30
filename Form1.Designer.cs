dad37d823b1f79416c10208351d828bb2464467a
DungMai <91529785+DungMaiTien@users.noreply.github.com>
2022-05-30 20:25:22 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace GamesSnake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.PauseButton = new System.Windows.Forms.Button();
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.LevelLabel = new System.Windows.Forms.Label();
+            this.DiemLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.lbdiemcaonhat = new System.Windows.Forms.Label();
+            this.Hdchoi = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(9, 15);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(673, 625);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ResetButton);
+            this.groupBox1.Controls.Add(this.PauseButton);
+            this.groupBox1.Controls.Add(this.PlayButton);
+            this.groupBox1.Location = new System.Drawing.Point(705, 117);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(200, 220);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tùy Chọn";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(23, 153);
+            this.ResetButton.Margin = new System.Windows.Forms.Padding(4);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(155, 45);
+            this.ResetButton.TabIndex = 2;
+            this.ResetButton.Text = "Làm Mới";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // PauseButton
+            // 
+            this.PauseButton.Enabled = false;
+            this.PauseButton.Location = new System.Drawing.Point(23, 95);
+            this.PauseButton.Margin = new System.Windows.Forms.Padding(4);
+            this.PauseButton.Name = "PauseButton";
+            this.PauseButton.Size = new System.Drawing.Size(155, 45);
+            this.PauseButton.TabIndex = 1;
+            this.PauseButton.Text = "Tạm dừng";
+            this.PauseButton.UseVisualStyleBackColor = true;
+            this.PauseButton.Click += new System.EventHandler(this.PauseButton_Click);
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.Location = new System.Drawing.Point(23, 41);
+            this.PlayButton.Margin = new System.Windows.Forms.Padding(4);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(155, 45);
+            this.PlayButton.TabIndex = 0;
+            this.PlayButton.Text = "Click để chơi ngay";
+            this.PlayButton.UseVisualStyleBackColor = true;
+            this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.LevelLabel);
+            this.groupBox2.Controls.Add(this.DiemLabel);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(705, 357);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(200, 151);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Chi tiết";
+            // 
+            // LevelLabel
+            // 
+            this.LevelLabel.AutoSize = true;
+            this.LevelLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LevelLabel.ForeColor = System.Drawing.Color.BlueViolet;
+            this.LevelLabel.Location = new System.Drawing.Point(92, 86);
+            this.LevelLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LevelLabel.Name = "LevelLabel";
+            this.LevelLabel.Size = new System.Drawing.Size(23, 25);
+            this.LevelLabel.TabIndex = 4;
+            this.LevelLabel.Text = "1";
+            this.LevelLabel.Click += new System.EventHandler(this.LevelLabel_Click);
+            // 
+            // DiemLabel
+            // 
+            this.DiemLabel.AutoSize = true;
+            this.DiemLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DiemLabel.ForeColor = System.Drawing.Color.BlueViolet;
+            this.DiemLabel.Location = new System.Drawing.Point(95, 39);
+            this.DiemLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.DiemLabel.Name = "DiemLabel";
+            this.DiemLabel.Size = new System.Drawing.Size(23, 25);
+            this.DiemLabel.TabIndex = 3;
+            this.DiemLabel.Text = "0";
+            this.DiemLabel.Click += new System.EventHandler(this.DiemLabel_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(17, 86);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cấp độ:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(17, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Điểm:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.panel1);
+            this.groupBox3.Location = new System.Drawing.Point(7, 2);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Size = new System.Drawing.Size(691, 647);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.lbdiemcaonhat);
+            this.groupBox4.Location = new System.Drawing.Point(705, 527);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(188, 122);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Điểm cao nhất:";
+            this.groupBox4.Enter += new System.EventHandler(this.groupBox4_Enter);
+            // 
+            // lbdiemcaonhat
+            // 
+            this.lbdiemcaonhat.AutoSize = true;
+            this.lbdiemcaonhat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbdiemcaonhat.ForeColor = System.Drawing.Color.Magenta;
+            this.lbdiemcaonhat.Location = new System.Drawing.Point(95, 39);
+            this.lbdiemcaonhat.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbdiemcaonhat.Name = "lbdiemcaonhat";
+            this.lbdiemcaonhat.Size = new System.Drawing.Size(23, 25);
+            this.lbdiemcaonhat.TabIndex = 5;
+            this.lbdiemcaonhat.Text = "0";
+            this.lbdiemcaonhat.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Hdchoi
+            // 
+            this.Hdchoi.Location = new System.Drawing.Point(705, 17);
+            this.Hdchoi.Name = "Hdchoi";
+            this.Hdchoi.Size = new System.Drawing.Size(200, 60);
+            this.Hdchoi.TabIndex = 4;
+            this.Hdchoi.Text = "Hướng dẫn chơi";
+            this.Hdchoi.UseVisualStyleBackColor = true;
+            this.Hdchoi.Click += new System.EventHandler(this.Hdchoi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 656);
+            this.Controls.Add(this.Hdchoi);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Game Snake";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.Button PauseButton;
+        private System.Windows.Forms.Button PlayButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LevelLabel;
+        private System.Windows.Forms.Label DiemLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label lbdiemcaonhat;
+        private System.Windows.Forms.Button Hdchoi;
+    }
+}
+
